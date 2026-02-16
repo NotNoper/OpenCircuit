4d931e4688c6d84ee5fa9b496123692d09483601
NotNoper <n.attilako@gmail.com>
2026-02-16 11:21:50 +0200

    test

--- frontend/datasheets-app.js
+++ frontend/datasheets-app.js
@@ -108,6 +108,8 @@ function DatasheetsApp() {
                             </button>
                         </div>
 
+                        <div className="flex justify-center"> <button class="bg-sky-950 text-sky-400 border border-sky-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group" onClick={() => GetListData()}> <span class="bg-sky-400 shadow-sky-400 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span> Submit </button> </div>
+
                         <div className="bg-slate-800/80 border border-slate-700 rounded-2xl p-6 shadow-xl backdrop-blur-md ring-1 ring-cyan-500/20 overflow-x-auto">
 
                             {results.length === 0 && !loading && (
