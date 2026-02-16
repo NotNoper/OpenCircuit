11eb2d343d5f702c4255a2754d7fd2a175b780cf
NotNoper <n.attilako@gmail.com>
2026-02-16 11:24:28 +0200

    test2

--- frontend/datasheets-app.js
+++ frontend/datasheets-app.js
@@ -1,39 +1,3 @@
-function GetListData() {
-    const promptEl = document.getElementById("projectPrompt");
-    if (!promptEl.value) {
-        alert("Enter a component.");
-        return;
-    }
-    console.log(promptEl.value);
-    SearchDatasheetInformation(promptEl.value);
-}
-
-async function SearchDatasheetInformation(componentName) {
-    try {
-        const response = await fetch(
-            "https://nikovision.onrender.com/search-datasheet-information",
-            {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ componentName: componentName }),
-            }
-        );
-
-        const result = await response.json();
-        const textOutput = document.getElementById("outputText");
-        textOutput.innerHTML = "";
-
-        const pre = document.createElement("pre");
-        pre.textContent = JSON.stringify(result, null, 2);
-        textOutput.appendChild(pre);
-
-        
-    } catch (err) {
-        console.error("Failed:", err);
-        alert("Failed.");
-    }
-}
-
 function DatasheetsApp() {
     try {
         const [search, setSearch] = React.useState('');
@@ -107,8 +71,6 @@ function DatasheetsApp() {
                             {loading ? "Searching..." : "Search"}
                             </button>
                         </div>
-
-                        <div className="flex justify-center"> <button class="bg-sky-950 text-sky-400 border border-sky-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group" onClick={() => GetListData()}> <span class="bg-sky-400 shadow-sky-400 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span> Submit </button> </div>
 
                         <div className="bg-slate-800/80 border border-slate-700 rounded-2xl p-6 shadow-xl backdrop-blur-md ring-1 ring-cyan-500/20 overflow-x-auto">
 
